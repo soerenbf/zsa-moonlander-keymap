45f073d65e9e1664383c601614c5d28e733d4cb2
Søren Bruus Zeppelin <soerenbf@gmail.com>
2024-11-26 11:56:16 +0100

    ..

--- Xewao/keymap.c
+++ Xewao/keymap.c
@@ -180,7 +180,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 void matrix_scan_user(void) {
   achordion_task();
 
-
 bool achordion_chord(uint16_t tap_hold_keycode,
                      keyrecord_t* tap_hold_record,
                      uint16_t other_keycode,
