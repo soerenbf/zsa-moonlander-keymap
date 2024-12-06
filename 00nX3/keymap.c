0c3b6dd7e738527c55b63eec381223c8c8fb6ae4
Søren Bruus Zeppelin <soerenbf@gmail.com>
2024-12-06 21:51:43 +0100

    apply mods to hands down layout

--- 00nX3/keymap.c
+++ 00nX3/keymap.c
@@ -3,6 +3,9 @@
 #define MOON_LED_LEVEL LED_LEVEL
 #define ML_SAFE_RANGE SAFE_RANGE
 
+#include "features/achordion.h"
+#include "features/custom_shift_keys.h"
+
 enum custom_keycodes {
   RGB_SLD = ML_SAFE_RANGE,
 };
@@ -118,8 +121,12 @@ bool rgb_matrix_indicators_user(void) {
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
+  // Achordion
+  if (!process_achordion(keycode, record)) { return false; }
+  // custom shift keys
+  if (!process_custom_shift_keys(keycode, record)) { return false; }
 
+  switch (keycode) {
     case RGB_SLD:
         if (rawhid_state.rgb_control) {
             return false;
@@ -133,4 +140,65 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 
+const uint16_t HOME_T = LCTL_T(KC_T);
+const uint16_t HOME_S = LGUI_T(KC_S);
+const uint16_t LT_BKSP = LT(1,KC_BSPC);
 
+// Achordion
+// https://getreuer.info/posts/keyboards/achordion/index.html
+void matrix_scan_user(void) {
+  achordion_task();
+}
+
+bool achordion_chord(uint16_t tap_hold_keycode,
+                     keyrecord_t* tap_hold_record,
+                     uint16_t other_keycode,
+                     keyrecord_t* other_record) {
+  // Exceptionally consider the following chords as holds
+  switch (tap_hold_keycode) {
+    case HOME_T:  // CTRL + D.
+      if (other_keycode == KC_D) { return true; }
+      break;
+    case HOME_S:  // GUI + C/V/Z/W.
+      if (other_keycode == KC_C || other_keycode == KC_V || other_keycode == KC_Z || other_keycode == KC_W) { return true; }
+      break;
+  }
+
+  // Also allow same-hand holds when the other key is in the rows below the
+  // alphas. I need the `% (MATRIX_ROWS / 2)` because my keyboard is split.
+  if (other_record->event.key.row % (MATRIX_ROWS / 2) >= 3) { return true; }
+
+  // Otherwise, follow the opposite hands rule.
+  return achordion_opposite_hands(tap_hold_record, other_record);
+}
+
+bool achordion_eager_mod(uint8_t mod) {
+  switch (mod) {
+    case MOD_LSFT:
+    case MOD_RSFT:
+    case MOD_LCTL:
+    case MOD_RCTL:
+    case MOD_LALT:
+    case MOD_RALT:
+    case MOD_LGUI:
+    case MOD_RGUI:
+      return true;  // Eagerly apply Shift, Alt, and Ctrl mods.
+
+    default:
+      return false;
+  }
+}
+
+// custom shift keys
+// https://getreuer.info/posts/keyboards/custom-shift-keys/index.html
+const custom_shift_key_t custom_shift_keys[] = {
+  {LT_BKSP, KC_DEL}, // Shift bksp is del
+  {KC_DOT , KC_COLN}, // Shift . is :
+  {KC_COMM, KC_SCLN}, // Shift , is ;
+  {KC_EQL , KC_EXLM}, // Shift = is !
+  {KC_PLUS, KC_ASTR}, // Shift + is *
+  {KC_AT  , KC_HASH}, // Shift @ is #
+  {KC_AMPR, KC_PIPE}, // Shift & is |
+};
+uint8_t NUM_CUSTOM_SHIFT_KEYS =
+    sizeof(custom_shift_keys) / sizeof(custom_shift_key_t);
